726de21be8c56ffdc3e8701fa04ba14b7ef16a89
jonas-94 <jonas_2010@hotmail.com>
2020-09-04 13:11:41 +0200

    klar

--- Uppgift1/Felande/Program.cs
+++ Uppgift1/Felande/Program.cs
@@ -12,9 +12,9 @@ namespace Felande
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
-        { 
-        
-            
+        {
+
+
 
             Console.WriteLine("Hej, välkommen till uppgift 1!");
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
@@ -27,26 +27,28 @@ namespace Felande
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
             Console.ReadKey();
 
+            bool sant = false;
+
             Console.WriteLine();
             Console.WriteLine($"Boolean eller bool är en användbar datatyp när man vill avgör om något är sant eller falskt. Här ser vi variablen sants värde: {sant}");
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
             Console.ReadKey();
-            
 
-           
-            bool sant = false;
+
+
+
 
             DateTime datum = DateTime.Now;
-            
+
             Console.WriteLine();
-            Console.ReadKey($"Här ser du ett datum representerat av variablen 'datum': {datum}");
+            Console.WriteLine($"Här ser du ett datum representerat av variablen 'datum': {datum}");
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
             Console.ReadKey();
 
             decimal decimaltal = 0.5M;
 
             Console.WriteLine();
-            Console.WriteLine($"Här ser du ett flyttal representerat av variablen 'decimaltal': {decimaltal});
+            Console.WriteLine($"Här ser du ett flyttal representerat av variablen 'decimaltal': {decimaltal}");
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
 
             string helttal = "30";
@@ -58,4 +60,5 @@ namespace Felande
 
             Console.ReadKey();
         }
+    }
 }
